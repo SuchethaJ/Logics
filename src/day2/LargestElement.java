4dec58c84852ee27fb294c17971a496b94a69e65
ADMIN <suchethaj13@gmail.com>
2025-11-04 17:01:43 +0530

    update 04-11-2025

---
+++ src/day2/LargestElement.java
@@ -0,0 +1,18 @@
+//Java Program to Print the Largest Element in an Integer Array without Using a Sorting Algorithm
+
+package day2;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,10,9,6,3};
+        int large=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>large){
+                large=arr[i];
+            }
+
+        }
+        System.out.println(large);
+    }
+
+}
---
+++ src/day2/SecondLargest.java
@@ -0,0 +1,20 @@
+//Java Program to Print the 2nd Largest Element in an Integer Array without Using a Sorting Algorithm
+package day2;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 10, 9, 6, 3};
+        int large = 0;
+        int seclarge = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > large) {
+                seclarge = large;
+                large = arr[i];
+            } else if (arr[i]>seclarge) {
+                seclarge=arr[i];
+            }
+        }
+        System.out.println(seclarge);
+    }
+}
