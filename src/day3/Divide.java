05a3cd2fd8bb171e4cdb6455d51bff2fb9dc655e
ADMIN <suchethaj13@gmail.com>
2025-11-05 17:03:36 +0530

    update 3

---
+++ src/day3/Divide.java
@@ -0,0 +1,18 @@
+package day3;
+public class Divide{
+    public static void main(String[] args) {
+        String str = "ABCDEFGHIJKL";
+        int n = 3; // divide into 3 parts
+
+        int length = str.length();
+        int size = length / n; // characters per part
+
+        if(length % n != 0) {
+            System.out.println("Cannot divide string equally!");
+        } else {
+            for(int i = 0; i < length; i += size) {
+                System.out.println(str.substring(i, i + size));
+            }
+        }
+    }
+}
